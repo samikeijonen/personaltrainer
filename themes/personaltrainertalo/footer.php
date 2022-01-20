e7a6520fb07aa66970dc388b15414ac88bf86e5c
Sami Keijonen <sami.keijonen@foxnet.fi>
2022-01-20 17:52:25 +0200

    Add aria label.

--- themes/personaltrainertalo/footer.php
+++ themes/personaltrainertalo/footer.php
@@ -9,13 +9,13 @@
     </div>
         </main><!-- .site-content -->
 
-        <section class="contact x-padding">
+        <section class="contact x-padding" aria-label="<?php esc_attr_e( 'Contact', 'kala' ); ?>">
             <div class="contact__container container top-margin">
             <?php
                     // Echo from reusable blocks `/wp-admin/edit.php?post_type=wp_block`.
                     // Needs to have slug `alapalkki`.
-                    $footer_post = get_page_by_path( 'lomake', '', 'wp_block' );
-                    if ( $footer_post ) :
+                    $form_post = get_page_by_path( 'lomake', '', 'wp_block' );
+                    if ( $form_post ) :
                         echo apply_filters( 'the_content', get_the_content( null, false, absint( $footer_post->ID )) ); // phpcs:ignore
                     endif;
                 ?>
