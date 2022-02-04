a912be4b5b46ba093e0d1c0d438e9649914bafeb
Sami Keijonen <sami.keijonen@foxnet.fi>
2022-02-04 14:08:28 +0200

    Add post types and block for all of them.

---
+++ plugins/meomblocks/blocks/latest-posts/components/sidebar.js
@@ -0,0 +1,73 @@
+const { __ } = wp.i18n;
+const { PanelBody, SelectControl, RadioControl } = wp.components;
+const { InspectorControls } = wp.blockEditor;
+
+/**
+ * Internal dependencies
+ */
+import TermSelector from './../../../components/term-selector';
+
+const Sidebar = (props) => {
+    const {
+        attributes: { postType, count },
+        setAttributes,
+    } = props;
+
+    return (
+        <InspectorControls>
+            <PanelBody title={__('Settings', 'meomblocks')} initialOpen={true}>
+                <SelectControl
+                    label={__('Select post type', 'meomblocks')}
+                    selected={postType}
+                    options={[
+                        {
+                            label: __('Post', 'meomblocks'),
+                            value: 'post',
+                        },
+                        {
+                            label: __('Reference', 'meomblocks'),
+                            value: 'reference',
+                        },
+                        {
+                            label: __('Professional', 'meomblocks'),
+                            value: 'professional',
+                        },
+                    ]}
+                    onChange={(newPostType) => {
+                        setAttributes({ postType: newPostType });
+                    }}
+                />
+
+                <RadioControl
+                    label={__('Select count', 'meomblocks')}
+                    selected={count}
+                    options={[
+                        {
+                            label: __('3', 'meomblocks'),
+                            value: '3',
+                        },
+                        {
+                            label: __('6', 'meomblocks'),
+                            value: '6',
+                        },
+                        {
+                            label: __('9', 'meomblocks'),
+                            value: '9',
+                        },
+                        {
+                            label: __('All', 'meomblocks'),
+                            value: '60',
+                        },
+                    ]}
+                    onChange={(newCount) => {
+                        setAttributes({ count: newCount });
+                    }}
+                />
+
+                <TermSelector {...props} />
+            </PanelBody>
+        </InspectorControls>
+    );
+};
+
+export default Sidebar;
--- plugins/meomblocks/blocks/latest-posts/frontend.php
+++ plugins/meomblocks/blocks/latest-posts/frontend.php
@@ -2,7 +2,18 @@
 
 namespace MEOM\Blocks;
 
-$class_name = attr( 'className', $attributes, '' );
+$post_type = attr( 'postType', $attributes, 'post' );
+$term_id   = attr( 'termId', $attributes );
+$count     = attr( 'count', $attributes, '3' );
+
+// Set taxonomy based on post type.
+$taxonomy = 'category';
+if ($post_type === 'reference') {
+    $taxonomy = 'reference_category';
+}
+if ($post_type === 'professional') {
+    $taxonomy = 'professional_category';
+}
 
 $class_names = [
     'latest-posts',
@@ -11,12 +22,22 @@ $class_names = [
 
 // Same base arguments for all.
 $args = [
-    'post_type'              => 'post',
-    'posts_per_page'         => 3,
+    'post_type'              => $post_type,
+    'posts_per_page'         => $count,
     'no_found_rows'          => true,
     'update_post_meta_cache' => false,
     'update_post_term_cache' => false,
 ];
+
+if ( $term_id && $term_id !== 0 ) :
+    $args['tax_query'] = [
+        [
+            'taxonomy' => esc_attr( $taxonomy ),
+            'field'    => 'id',
+            'terms'    => absint( $term_id ),
+        ],
+    ];
+endif;
 
 $latest_posts = new \WP_Query( $args );
 
